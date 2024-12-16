21863d6f4ea83d97e502c08d8fe3f0dda0832955
GaeulLee <rhakq23@gmail.com>
2024-12-16 19:24:22 +0900

    Initial Commit

---
+++ CalendarMemo/CalendarMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarMemoApp.swift
+//  CalendarMemo
+//
+//  Created by 이가을 on 12/16/24.
+//
+
+import SwiftUI
+
+@main
+struct CalendarMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

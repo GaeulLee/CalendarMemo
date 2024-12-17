373939bf996ed1bf0ffa1c87ad3225244eec9b1c
GaeulLee <rhakq23@gmail.com>
2024-12-17 17:39:21 +0900

    Onboarding

--- CalendarMemo/Feature/Onboarding/OnboardingView.swift
+++ CalendarMemo/Feature/Onboarding/OnboardingView.swift
@@ -8,8 +8,128 @@
 import SwiftUI
 
 struct OnboardingView: View {
+    @StateObject private var onboardingVM = OnboardingViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        OnboardingContentView(onboardingVM: onboardingVM)
+        
+        // navi
+        
+
+    }
+}
+// MARK: - OnboardingContent
+private struct OnboardingContentView: View {
+    @ObservedObject private var onboardingVM : OnboardingViewModel
+    
+    fileprivate init(onboardingVM: OnboardingViewModel) {
+        self.onboardingVM = onboardingVM
+    }
+    
+    fileprivate var body: some View {
+        ZStack {
+            VStack {
+                IndicatorView(onboardingVM: onboardingVM)
+                
+                OnboardingTabView(onboardingVM: onboardingVM)
+            }
+            
+            StartBtnView()
+        }
+    }
+}
+// MARK: - Indicator
+private struct IndicatorView: View {
+    @ObservedObject private var onboardingVM : OnboardingViewModel
+    
+    fileprivate init(onboardingVM: OnboardingViewModel) {
+        self.onboardingVM = onboardingVM
+    }
+    
+    fileprivate var body: some View {
+        HStack(spacing: 8) {
+            ForEach(0..<onboardingVM.contents.count, id: \.self) { index in
+                Circle()
+                    .frame(width: 8, height: 8)
+                    .foregroundColor(index == onboardingVM.selectedIndex 
+                                     ? .customDarkGreen
+                                     : .customDarkGreen.opacity(0.3))
+            }
+        }
+        .padding(.top, 20)
+    }
+}
+
+// MARK: - OnboardingTab
+private struct OnboardingTabView: View {
+    @ObservedObject private var onboardingVM : OnboardingViewModel
+    
+    fileprivate init(onboardingVM: OnboardingViewModel) {
+        self.onboardingVM = onboardingVM
+    }
+    
+    fileprivate var body: some View {
+        TabView(selection: $onboardingVM.selectedIndex) {
+            ForEach(Array(onboardingVM.contents.enumerated()), id: \.element) { index, content in
+                OnboardingCellView(content: content)
+                    .tag(index)
+            }
+        }
+        .tabViewStyle(.page(indexDisplayMode: .never))
+        
+    }
+}
+
+
+
+// MARK: - OnboardingCell
+private struct OnboardingCellView: View {
+    private var content: OnboardingContent
+    
+    fileprivate init(content: OnboardingContent) {
+        self.content = content
+    }
+    
+    fileprivate var body: some View {
+        VStack {
+            Text(content.content)
+                .font(.system(size: 20, weight: .bold))
+            
+            Image(content.imageFileName)
+                .resizable()
+                .frame(width: 310, height: 610)
+        }
+        .padding(.top, 30)
+    }
+}
+
+
+
+// MARK: - StartBtn
+private struct StartBtnView: View {
+    
+    fileprivate var body: some View {
+        VStack {
+            Spacer()
+            
+            HStack {
+                Button(
+                    action: {  },
+                    label: {
+                        HStack {
+                            Text("시작하기")
+                                .font(.system(size: 17, weight: .bold))
+                                .foregroundColor(.customDarkGreen)
+                            Image("rightArrow")
+                                .renderingMode(.template)
+                                .foregroundColor(.customDarkGreen)
+                        }
+                    }
+                )
+            }
+            .frame(width: 500, height: 65)
+            .background(.customBeige)
+        }
     }
 }
 
--- CalendarMemo/Feature/Onboarding/OnboardingViewModel.swift
+++ CalendarMemo/Feature/Onboarding/OnboardingViewModel.swift
@@ -8,14 +8,17 @@
 import Foundation
 
 class OnboardingViewModel: ObservableObject {
-    @Published var content: [OnboardingContent]
+    @Published var contents: [OnboardingContent]
+    @Published var selectedIndex: Int
     
     init(content: [OnboardingContent] = [
-        OnboardingContent(imageFileName: "onboarding1", content: "달력에 적는 나만의 메모"),
-        OnboardingContent(imageFileName: "onboarding2", content: "모든 메모가 한 눈에"),
-        OnboardingContent(imageFileName: "onboarding3", content: "메모 미리 알림까지")
-    ]
+            OnboardingContent(imageFileName: "onboarding1", content: "달력에 적는 나만의 메모"),
+            OnboardingContent(imageFileName: "onboarding2", content: "모든 메모가 한 눈에"),
+            OnboardingContent(imageFileName: "onboarding3", content: "메모 미리 알림까지")
+        ],
+         selectedIndex: Int = 0
     ) {
-        self.content = content
+        self.contents = content
+        self.selectedIndex = selectedIndex
     }
 }
